2c321e792670744d18fbee360f84a9928b083d40
Matheus Rossi <medeiroshrmh3@gmail.com>
2025-09-15 21:51:54 -0300

    Static

---
+++ EstaticosExer/src/main/java/Back/Calculator.java
@@ -0,0 +1,9 @@
+package Back;
+
+public class Calculator {
+
+    public static final double soma (double price, double quantity){
+        return price * quantity * 0.06 + price * quantity;
+    }
+
+}
---
+++ EstaticosExer/src/main/java/Front/Interface.java
@@ -0,0 +1,21 @@
+package Front;
+
+import Back.Calculator;
+
+import java.util.Scanner;
+
+public class Interface {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What is the dollar price? ");
+        double price = sc.nextDouble();
+
+        System.out.println("How many dollars will be bought? ");
+        double quantity = sc.nextDouble();
+
+        System.out.printf("Amount to be paid in reais = %.2f%n", Calculator.soma(price, quantity));
+
+        sc.close();
+    }
+}
